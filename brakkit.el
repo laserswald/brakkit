b561f6d98c3ce9ab2a0186ccd185fef75a52a776
Box O'Rocks <rocx@rocx.rocks>
2018-04-03 21:05:09 -0400

    new: mostly anything works in substitutions
    `replace-match' needs a string to replace matches with so now
    substitutions need to be "intelligent".
    
    The DEFNS argument in `brakkit-fill-string' also works with hash
    tables as well as alists.

--- brakkit.el
+++ brakkit.el
@@ -1,9 +1,27 @@
 (defun brakkit-fill-string (template defns)
-  "Fill in a TEMPLATE string using the values from the DEFINITIONS alist."
+  "Fill in a TEMPLATE string using the values from the DEFNS alist."
   (with-temp-buffer
     (insert template)
     (beginning-of-buffer)
     (while (search-forward-regexp
 	    "{{[[:space:]]*\\([[:word:]_\\-]+\\)[[:space:]]*}}" nil t )
-      (replace-match (alist-get (intern (match-string 1)) defns)))
+      (replace-match (brakkit--get-defn (intern (match-string 1)) defns)))
     (buffer-string)))
+
+(defun brakkit--alistp (list)
+  "Return t if LIST is an association list.
+
+For Brakkit, an association list is defined as a list that only
+contains cons cells (pairs)."
+  (when (listp list)
+    (cl-loop for cell in list
+	     always (consp cell))))
+
+(defun brakkit--get-defn (key defns)
+  "Intelligently extract a value (as a string) from DEFNS given a KEY.
+
+DEFNS can be either an association list or hash table."
+  (prin1-to-string (cond ((brakkit--alistp defns) (alist-get key defns))
+			 ((hash-table-p defns)    (gethash key defns)))))
+
+(provide 'brakkit)
